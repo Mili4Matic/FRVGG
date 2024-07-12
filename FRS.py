7fd5bd78c209d2b8e6f12b92e1b0266cb85f1b20
Mili4Matic <amilikfer@gmail.com>
2024-07-13 01:05:54 +0200

    final version

--- FRS.py
+++ FRS.py
@@ -1,3 +1,29 @@
+# Author: Alejandro Milikowsky
+# Email: alejandro.milikowsky@estudiante.uam.es
+# Date: July 2024
+#
+# This program is developed for real-time face recognition using a webcam.
+# It leverages the VGGFace model to extract facial features and compares them 
+# with known individuals' features stored in a specified directory.
+# The purpose is to identify known individuals and label them accordingly 
+# while marking unknown faces. The program also updates the known features 
+# with new captures if the recognition confidence is high.
+#
+# Directory Structure for Known Faces:
+# The known directory should contain subdirectories named after known individuals.
+# Each subdirectory should contain images of the person.
+# Example:
+# /home/mili/Desktop/FRTest/FRVGG/known/
+# ├── person1/
+# │   ├── image1.jpg
+# │   ├── image2.jpg
+# │   └── ...
+# ├── person2/
+# │   ├── image1.jpg
+# │   ├── image2.png
+# │   └── ...
+# └── ...
+
 from keras.engine import Model
 from keras.layers import Flatten
 from keras_vggface.vggface import VGGFace
@@ -8,25 +34,25 @@ import cv2
 import os
 from scipy.spatial.distance import cosine
 
-# loading only feature extractor layers (chekear documentacion: https://github.com/rcmalli/keras-vggface)
-vgg_model = VGGFace(include_top=False, input_shape=(224, 224, 3), pooling='avg') #pooling: max, avg, min
+# loading only feature extractor layers (check documentation: https://github.com/rcmalli/keras-vggface)
+vgg_model = VGGFace(include_top=False, input_shape=(224, 224, 3), pooling='avg')  # pooling: max, avg, min
 
 # cv Face Detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Pretraining model function with our faces on our directories
+# Preprocessing model function with our faces on our directories
 def preprocess_image(img):
     img = cv2.resize(img, (224, 224)).astype('float32')
     img = np.expand_dims(img, axis=0)
-    img = utils.preprocess_input(img, version=1)  # version=1 for VGGFace1, version=2 for VGGFace2, !!Versino 1 for VGG16, Version 2 for RESNET AND SENET!!
+    img = utils.preprocess_input(img, version=1)  # version=1 for VGGFace1, version=2 for VGGFace2, !!Version 1 for VGG16, Version 2 for RESNET AND SENET!!
     return img
 
-# feature extraction using vgg model from the image
+# Feature extraction using vgg model from the image
 def extract_features(model, img):
     features = model.predict(img)
-    return features.flatten()  # 1D flatterend
+    return features.flatten()  # 1D flattened
 
-# Got help formm chatGPT on this one, check full function again | Load person and known features
+# Load person and known features
 def load_known_features(known_images_dir):
     known_features = {}
     for person in os.listdir(known_images_dir):
@@ -41,7 +67,7 @@ def load_known_features(known_images_dir):
     return known_features
 
 # Check and label person
-def classify_person(webcam_features, known_features, similarity_threshold=0.65):
+def classify_person(webcam_features, known_features, similarity_threshold=0.68):
     min_distance = float('inf')
     best_match = None
     for person, features_list in known_features.items():
@@ -57,34 +83,15 @@ def classify_person(webcam_features, known_features, similarity_threshold=0.65):
 def update_known_features(known_features, person, new_features):
     known_features[person].append(new_features)
 
-
-
 # Load known images features
-# Directory Structure for Known Faces:
-# The known directory should contain subdirectories named after known individuals.
-# Each subdirectory should contain images of the person.
-# Example:
-# /home/mili/Desktop/FRTest/FRVGG/known/
-# ├── person1/
-# │   ├── image1.jpg
-# │   ├── image2.jpg
-# │   └── ...
-# ├── person2/
-# │   ├── image1.jpg
-# │   ├── image2.png
-# │   └── ...
-# └── ...
 known_images_dir = '/home/mili/Desktop/FRTest/FRVGG/known'
 known_features = load_known_features(known_images_dir)
-
-
 
 # Webcam function
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("No se pudo abrir la webcam.")
     exit()
-
 
 while True:
     ret, frame = cap.read()
@@ -104,8 +111,8 @@ while True:
         label = "Unknown"
         percentage = (1 - min_distance) * 100
         
-        #Check if i know him, known if percentage is >65, update feaures with captured image if percentage is >80
-        if known and percentage > 65:
+        # Check if I know him, known if percentage is >65, update features with captured image if percentage is >80
+        if known and percentage > 70:
             label = f'{person} ({percentage:.2f}%)'
             if percentage > 80:
                 update_known_features(known_features, person, face_features)
